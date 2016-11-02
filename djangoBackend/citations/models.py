5871f7796c2c7aff64549dc03d58adbbe5d1e7f9
Miron Zelina <mironzelinajr@gmail.com>
2016-11-02 13:45:45 +0000

    Add models for citations and users
    Django models for citations and users. These models are not yet reflected
    in the database, just in python code.

--- djangoBackend/citations/models.py
+++ djangoBackend/citations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 32)
+    passwordHash = models.CharField(max_length = 1024)
+
+class Citation(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 256)
+    link = models.CharField(max_length = 1024)
+    note = models.CharField(max_length = 65536, default = '')
