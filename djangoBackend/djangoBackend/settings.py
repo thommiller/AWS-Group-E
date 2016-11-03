879127d12447f009a2cd86ea99b8286f5da542f6
Miron Zelina <mironzelinajr@gmail.com>
2016-11-03 23:29:24 +0000

    Static url fix
    Source directories for static files: /assets, /www, /djangoBackend/static.
    Output directories for static files: /staticFiles.

--- djangoBackend/djangoBackend/settings.py
+++ djangoBackend/djangoBackend/settings.py
@@ -165,14 +165,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticFiles")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "..", "www"),
     os.path.join(BASE_DIR, "..", "assets"),
+    os.path.join(BASE_DIR, "static"),
 ]
-
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_URL = '/'
