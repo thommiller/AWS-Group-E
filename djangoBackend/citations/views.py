ade413f00aaa85264315ae13e7d458333359a379
Miron Zelina <mironzelinajr@gmail.com>
2016-11-03 16:53:52 +0000

    Move signup-page to templates

--- djangoBackend/citations/views.py
+++ djangoBackend/citations/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test response.")
+    return render(request, template_name = "signup-page.html")
--- djangoBackend/djangoBackend/settings.py
+++ djangoBackend/djangoBackend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../www/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
