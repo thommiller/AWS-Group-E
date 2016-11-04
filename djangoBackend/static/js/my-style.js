2395d0fb1bd222b04ce8a2ca273caa169e071994
Thom <zcabtme@ucl.ac.uk>
2016-11-04 12:13:20 +0000

    added search functionality in my-style.js using jQuery filtering

--- djangoBackend/static/js/my-style.js
+++ djangoBackend/static/js/my-style.js
@@ -45,6 +45,40 @@ gsdk = {
     },
 }
 
+$(".form-control").keyup(function () {
+    //split the current value of searchInput
+    var data = this.value.split(" ");
+    //create a jquery object of the rows
+    var jo = $(".content").find("tr");
+    if (this.value == "") {
+        jo.show();
+        return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
+            }
+        }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+}).focus(function () {
+    this.value = "";
+    $(this).css({
+        "color": "black"
+    });
+    $(this).unbind('focus');
+}).css({
+    "color": "#C0C0C0"
+});
+
 $('button[id="rowDelete"]').click(function(e){
    $(this).closest('tr').remove()
 })
