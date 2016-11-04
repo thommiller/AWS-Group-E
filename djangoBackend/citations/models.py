bdf579ba3315fe751d46eee59d51c04631b85682
Tilman Schmidt <AccessKeymaster@gmx.net>
2016-11-04 15:32:47 +0000

    Changed default fields for Citations.

--- djangoBackend/citations/models.py
+++ djangoBackend/citations/models.py
@@ -7,10 +7,10 @@ DATE_FORMATS = ['%d/%m/%Y',]
 
 class Citation(models.Model):
     user = models.ForeignKey(User)
-    author_fname = models.CharField(max_length = 128, default = '0000000')
-    author_lname = models.CharField(max_length = 128, default = '0000000')
-    title = models.CharField(max_length = 256, default = '0000000')
-    link = models.CharField(max_length = 1024, default = '0000000')
-    notes = models.CharField(max_length = 65536, default = '0000000')
-    date_pub = models.DateField(default="1970-01-01")
-    date_acc = models.DateField(default="1970-01-01")
+    author_fname = models.CharField(max_length = 128, default = '')
+    author_lname = models.CharField(max_length = 128, default = '')
+    title = models.CharField(max_length = 256, default = '')
+    link = models.CharField(max_length = 1024, default = '')
+    notes = models.CharField(max_length = 65536, default = '')
+    date_pub = models.DateField(default="2000-01-01")
+    date_acc = models.DateField(default="2000-01-01")
